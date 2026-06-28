bca090949e3a1cc9a9497533445a60a89315793a
Volkan Güler <volkangueler54@gmail.com>
2026-06-29 01:10:59 +0200

    Add polling to request and wait for model generation

--- generate_model.py
+++ generate_model.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 from dotenv import load_dotenv
 
 #Starts an Image-to-3D task with meshy and provides the task-id.
@@ -27,6 +28,27 @@ print("Status code:", response.status_code)
 if 200 <= response.status_code < 300:
     task_id = response.json()["result"]
     print("Task started. task_id:", task_id)
+
+    status_url = f"https://api.meshy.ai/openapi/v1/image-to-3d/{task_id}"
+
+    while True:
+        status_response= requests.get(status_url, headers=headers)
+        task = status_response.json()
+        status = task["status"]
+        progress = task.get("progress", 0)
+        print(f" Status: {status} ({progress}%)")
+
+        if status == "SUCCEEDED":
+            print("Download-URL:", task["model_urls"]["glb"])
+            break
+        if status == "FAILED":
+            print("Generation failed.")
+            break
+
+        time.sleep(5)
+    
+    """Checking repeatedly for the generation status"""
+
 else:
     print("Something went wrong. Answer from server:")
     print(response.text)
