90faa4085551a33e909437ba0b1424b90929164a
Volkan Güler <volkangueler54@gmail.com>
2026-07-01 16:09:11 +0200

    Switch gen backend from meshy to fal

--- main.py
+++ main.py
@@ -1,47 +1,23 @@
-import requests
 import os
-import time
+import fal_client
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 load_dotenv()
 API_KEY= os.getenv("MESHY_API_KEY")
 
 app = FastAPI()
 
-headers = {
-    "Authorization": f"Bearer {API_KEY}"
-}
-
 @app.get("/")
 def read_root():
     return {"message": "Rextrude server is running."}
 
 @app.post("/generate")
-def generate_model():
-    payload = {
-        "image_url": "https://picsum.photos/512",
-        "target_formats": ["glb"],
-    }
+def generate_model(image: UploadFile = File(...)):
+    image_bytes = image.file.read()
+    image_url = fal_client.upload(image_bytes, image.content_type)
 
-    response = requests.post(
-        "https://api.meshy.ai/openapi/v1/image-to-3d",
-        headers=headers,
-        json=payload
-    )
-    response.raise_for_status()
-    task_id = response.json()["result"]
-    status_url = f"https://api.meshy.ai/openapi/v1/image-to-3d/{task_id}"
-    while True:
-        status_response= requests.get(status_url, headers=headers)
-        task = status_response.json()
-        status = task["status"]
+    result = fal_client.subscribe("fal-ai/trellis", arguments={"image_url": image_url},)
 
-        if status == "SUCCEEDED":
-            glb_url = task["model_urls"]["glb"]
-            return {"model_url": glb_url}
-         
-        if status == "FAILED":
-            return {"error": "Generation failed"}
-            
-        time.sleep(5)         #Checking repeatedly for the generation status
+    return {"model_url": result["model_mesh"]["url"]}
+    
